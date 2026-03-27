ba92d8fbf87b8f6af8c6a4d30da4aafb91b0f45f
steveng9 <golobs@uw.edu>
2026-03-27 00:00:43 -0700

    tune

--- js/Constants.js
+++ js/Constants.js
@@ -90,8 +90,8 @@ Constants = {
 
     /* Zerlin health and force stats*/
     Z_MAX_HEALTH: 10, //was 20
-    Z_MAX_FORCE: 10,
-    Z_FORCE_REGEN_PER_SECOND: 1/15,
+    Z_MAX_FORCE: 15,
+    Z_FORCE_REGEN_PER_SECOND: 1/7,
     Z_FORCE_JUMP_FORCE_COST: 3,
     Z_SOMERSAULT_FORCE_COST: 3,
     Z_SABER_THROW_FORCE_COST: 4,
